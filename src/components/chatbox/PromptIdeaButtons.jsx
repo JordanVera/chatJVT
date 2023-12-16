a11256d7030978a95c54664839df912c2de3eaee
Jordan Vera <verawebdevelopment@gmail.com>
2023-12-16 11:22:59 -0600

    Add PromptIdeaButtons component to WelcomeMessage

---
+++ src/components/chatbox/PromptIdeaButtons.jsx
@@ -0,0 +1,79 @@
+import { useState } from 'react';
+import { IconButton } from '@mui/material';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+
+const PromptIdeaButtons = () => {
+  const data = [
+    { title: 'Help me debug', subtitle: 'a linked list problem' },
+    {
+      title: 'Design a database schema',
+      subtitle: 'for an online ecommerce store',
+    },
+    {
+      title: 'Plan an itinerary',
+      subtitle: 'for a fashion focused vacation to Paris',
+    },
+    { title: 'Create a workout plan', subtitle: 'for resistance training' },
+  ];
+
+  const [hoveredButton, setHoveredButton] = useState(null);
+
+  const handleButtonHover = (index) => {
+    setHoveredButton(index);
+  };
+
+  const handleButtonLeave = () => {
+    setHoveredButton(null);
+  };
+
+  return (
+    <div
+      id="suggestedPrompts"
+      className="w-full mb-3 px-6 space-y-2 flex-grow flex flex-col justify-end"
+    >
+      <div className="flex flex-col sm:flex-row w-full sm:space-x-2">
+        {data.slice(0, 2).map((item, index) => (
+          <button
+            key={index}
+            className="border border-gray-500 w-full p-4 rounded-2xl mx-2 sm:mx-0 text-left hover:bg-[#40414F] relative flex flex-row justify-between"
+            onMouseEnter={() => handleButtonHover(index)}
+            onMouseLeave={handleButtonLeave}
+          >
+            <div>
+              <h4 className="text-white text-sm font-bold">{item.title}</h4>
+              <h5 className="text-gray-500 text-xs">{item.subtitle}</h5>
+            </div>
+            {hoveredButton === index && (
+              <button className="px-2 bg-gray-900 rounded-full h-8 w-8 pb-[3px]">
+                <ArrowUpwardIcon style={{ height: 13, width: 13 }} />
+              </button>
+            )}
+          </button>
+        ))}
+      </div>
+
+      <div className="hidden sm:flex flex-row w-full space-x-2 ">
+        {data.slice(2, 4).map((item, index) => (
+          <button
+            key={index}
+            className="border border-gray-500 w-full p-4 rounded-2xl text-left hover:bg-[#40414F] relative flex flex-row justify-between"
+            onMouseEnter={() => handleButtonHover(index + 2)}
+            onMouseLeave={handleButtonLeave}
+          >
+            <div>
+              <h4 className="text-white text-sm font-bold">{item.title}</h4>
+              <h5 className="text-gray-500 text-xs">{item.subtitle}</h5>
+            </div>
+            {hoveredButton === index + 2 && (
+              <button className="mt-1 bg-gray-900 rounded-full h-8 w-8 pb-[3px]">
+                <ArrowUpwardIcon style={{ height: 13, width: 13 }} />
+              </button>
+            )}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PromptIdeaButtons;
--- src/components/chatbox/WelcomeMessage.jsx
+++ src/components/chatbox/WelcomeMessage.jsx
@@ -1,4 +1,5 @@
 import { Avatar } from '@mui/material';
+import PromptIdeaButtons from './PromptIdeaButtons';
 
 const WelcomeMessage = () => {
   return (
@@ -39,32 +40,7 @@ const WelcomeMessage = () => {
         </h2>
       </div>
 
-      <div
-        id="suggestedPrompts"
-        className="w-full mb-3 px-6 space-y-2 flex-grow flex flex-col justify-end"
-      >
-        <div className="flex flex-col sm:flex-row w-full sm:space-x-2 ">
-          <div className="border border-gray-500 w-full p-4 rounded-2xl mx-2 sm:mx-0">
-            <h4 className="text-white text-sm font-bold">Help me debug</h4>
-            <h5 className="text-gray-500 text-xs">a linked list problem</h5>
-          </div>
-
-          <div className="border border-gray-500 w-full p-4 rounded-2xl mx-2 sm:mx-0">
-            <h4 className="text-white text-sm font-bold">Help me debug</h4>
-            <h5 className="text-gray-500 text-xs">a linked list problem</h5>
-          </div>
-        </div>
-        <div className="hidden sm:block sm:flex flex-row w-full space-x-2 ">
-          <div className="border border-gray-500 w-full p-4 rounded-2xl">
-            <h4 className="text-white text-sm font-bold">Help me debug</h4>
-            <h5 className="text-gray-500 text-xs">a linked list problem</h5>
-          </div>
-          <div className="border border-gray-500 w-full p-4 rounded-2xl">
-            <h4 className="text-white text-sm font-bold">Help me debug</h4>
-            <h5 className="text-gray-500 text-xs">a linked list problem</h5>
-          </div>
-        </div>
-      </div>
+      <PromptIdeaButtons />
     </div>
   );
 };
