31e122d827a8e146320315054c7e1d07d91ef49d
Jordan Vera <verawebdevelopment@gmail.com>
2023-12-15 22:51:26 -0600

    Refactor Answer component in chatbox

--- src/components/chatbox/Answer.jsx
+++ src/components/chatbox/Answer.jsx
@@ -20,7 +20,10 @@ const Answer = ({ loading, chats, selectedChat, messages, setMessages }) => {
                 <div className="responses " key={i}>
                   {/* USER MESSAGE */}
                   {message.role === 'user' ? (
-                    <div className="mb-8 py-2 flex flex-1 text-base mx-auto gap-3 md:px-5 lg:px-1 xl:px-5 md:max-w-3xl lg:max-w-[40rem] xl:max-w-[48rem] } group">
+                    <div
+                      id="user-responose"
+                      className="group py-2 flex flex-1 text-base mx-auto gap-3 md:px-5 lg:px-1 xl:px-5 md:max-w-3xl lg:max-w-[40rem] xl:max-w-[48rem] } group"
+                    >
                       <div className="flex-shrink-0 flex flex-col relative items-end">
                         <Avatar
                           alt="Vera"
@@ -36,10 +39,27 @@ const Answer = ({ loading, chats, selectedChat, messages, setMessages }) => {
                       </div>
                       <div className="relative flex w-full flex-col lg:w-[calc(100%-115px)] agent-turn">
                         <h2 className="text-white font-semibold">You</h2>
-                        <h3 key={i}>
+                        <p key={i} className="mb-2">
                           {message.content &&
                             capitalizeFirstLetter(message.content)}
-                        </h3>
+                        </p>
+
+                        {/* EDIT MSG BUTTON */}
+                        <svg
+                          width="16"
+                          height="16"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="hidden group-hover:block icon-md mb-4 text-gray-400 hover:text-white"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            clip-rule="evenodd"
+                            d="M13.2929 4.29291C15.0641 2.52167 17.9359 2.52167 19.7071 4.2929C21.4783 6.06414 21.4783 8.93588 19.7071 10.7071L18.7073 11.7069L11.1603 19.2539C10.7182 19.696 10.1489 19.989 9.53219 20.0918L4.1644 20.9864C3.84584 21.0395 3.52125 20.9355 3.29289 20.7071C3.06453 20.4788 2.96051 20.1542 3.0136 19.8356L3.90824 14.4678C4.01103 13.8511 4.30396 13.2818 4.7461 12.8397L13.2929 4.29291ZM13 7.41422L6.16031 14.2539C6.01293 14.4013 5.91529 14.591 5.88102 14.7966L5.21655 18.7835L9.20339 18.119C9.40898 18.0847 9.59872 17.9871 9.7461 17.8397L16.5858 11L13 7.41422ZM18 9.5858L14.4142 6.00001L14.7071 5.70712C15.6973 4.71693 17.3027 4.71693 18.2929 5.70712C19.2831 6.69731 19.2831 8.30272 18.2929 9.29291L18 9.5858Z"
+                            fill="currentColor"
+                          ></path>
+                        </svg>
                       </div>
                     </div>
                   ) : (
@@ -78,9 +98,11 @@ const Answer = ({ loading, chats, selectedChat, messages, setMessages }) => {
                       <div className="relative flex w-full flex-col lg:w-[calc(100%-115px)] agent-turn">
                         <h2 className="text-white font-semibold">ChatGPT</h2>
 
-                        {message.content && (
-                          <Typewriter text={message.content} key={i} />
-                        )}
+                        <p>
+                          {message.content && (
+                            <Typewriter text={message.content} key={i} />
+                          )}
+                        </p>
                       </div>
                     </div>
                   )}
